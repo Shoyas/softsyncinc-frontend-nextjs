72c05d0b862dce5a6466289f4b6cbd8ad50dc228
Rakibulislam1 <rirakib03@gmail.com>
2024-02-23 03:55:00 +0600

    swiper issue not fixed

--- src/components/ui/HomeUi/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/ui/HomeUi/WhyChooseUs/WhyChooseUs.tsx
@@ -1,18 +1,23 @@
 "use client"
 
-
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import iconOne from "../../../../asset/1.webp";
 import iconTwo from "../../../../asset/2.webp";
 import iconThree from "../../../../asset/3.webp";
 import iconFour from "../../../../asset/4.webp";
 import Image from "next/image";
+// Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/free-mode';
-import { Autoplay, FreeMode } from 'swiper/modules';
+import 'swiper/css/pagination';
+import {  Pagination } from 'swiper/modules';
+
+
 
 
 const WhyChooseArea = () => {
+
+
+
     return (
         <section className="px-3 lg:px-0 mt-24">
             <div className="grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-4">
@@ -25,47 +30,57 @@ const WhyChooseArea = () => {
                     </h4>
                 </div>
 
-                <div className="col-span-2">
+                <div className="lg:col-span-2">
                     <Swiper
+                        slidesPerView={1}
+                        spaceBetween={10}
+                        pagination={{
+                            clickable: true,
+                        }}
                         breakpoints={{
-                            340: {
+                            640: {
                                 slidesPerView: 2,
-                                spaceBetween: 10,
+                                spaceBetween: 20,
                             },
-                            700: {
-                                slidesPerView: 3,
-                                spaceBetween: 15,
+                            768: {
+                                slidesPerView: 4,
+                                spaceBetween: 40,
+                            },
+                            1024: {
+                                slidesPerView: 5,
+                                spaceBetween: 50,
                             },
                         }}
-                        freeMode={true}
-                        modules={[FreeMode, Autoplay]}
-                        // autoplay={{ delay: 2000 }}
-                        loop={true}
+                        modules={[Pagination]}
+                        className="mySwiper"
                     >
                         <SwiperSlide className='slide-width'>
                             <div className='flex flex-col gap-4 justify-center items-center'>
                                 <Image className="lg:w-32 lg:h-32 md:w-28 md:h-28 w-20 h-20" src={iconOne} alt="icon" />
-                                <p className="font-semibold text-[#453F41]">Fast Performance</p>
+                                <p className="font-semibold text-gray text-sm">Fast Performance</p>
                             </div>
                         </SwiperSlide>
+
                         <SwiperSlide className='slide-width'>
                             <div className='flex flex-col gap-4 justify-center items-center'>
                                 <Image className="lg:w-32 lg:h-32 md:w-28 md:h-28 w-20 h-20" src={iconTwo} alt="icon" />
-                                <p className="font-semibold text-[#453F41]">Standard Code</p>
+                                <p className="font-semibold text-gray text-sm">Standard Code</p>
                             </div>
                         </SwiperSlide>
+
                         <SwiperSlide className='slide-width'>
                             <div className='flex flex-col gap-4 justify-center items-center'>
                                 <Image className="lg:w-32 lg:h-32 md:w-28 md:h-28 w-20 h-20" src={iconThree} alt="icon" />
-                                <p className="font-semibold text-[#453F41]">
+                                <p className="font-semibold text-gray text-sm">
                                     Responsive Design
                                 </p>
                             </div>
                         </SwiperSlide>
+
                         <SwiperSlide className='slide-width'>
                             <div className='flex flex-col gap-4 justify-center items-center'>
                                 <Image className="lg:w-32 lg:h-32 md:w-28 md:h-28 w-20 h-20" src={iconFour} alt="icon" />
-                                <p className="font-semibold text-[#453F41]">Quick Support</p>
+                                <p className="font-semibold text-gray text-sm">Quick Support</p>
                             </div>
                         </SwiperSlide>
                     </Swiper>
